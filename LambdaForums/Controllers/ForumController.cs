34e5e5f001a6cda72af2f94d8881a0f80a008903
Robert W. Leonard <robert@draknor.com>
2017-12-09 17:16:23 -0600

    Add Basic Forum Views

---
+++ LambdaForums/Controllers/ForumController.cs
@@ -0,0 +1,43 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Data.Models;
+using LambdaForums.Models.Forum;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaForums.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll()
+                .Select(forum => new ForumListingModel {
+                    Id = forum.Id,
+                    Name = forum.Title,
+                    Description = forum.Description
+            });
+
+            var model = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Topic(int id)
+        {
+            var forum = _forumService.GetById(id);
+
+            return View();
+        }
+    }
+}
