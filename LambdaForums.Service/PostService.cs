08ff27790d362ef246188c356a28f08283b6ea37
Robert W. Leonard <robert@draknor.com>
2017-12-09 23:27:49 -0600

    Add Topic page, updated Data and service layer

---
+++ LambdaForums.Service/PostService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LambdaForums.Data;
+using LambdaForums.Data.Models;
+
+namespace LambdaForums.Service
+{
+    public class PostService : IPost
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Add(Post post)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task AddReply(PostReply reply)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task EditPostContent(int id, string newContent)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetFilteredPosts(string searchQuery)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetPostsByForum(int id)
+        {
+            return _context.Forums
+                .Where(forum => forum.Id == id).First()
+                .Posts;
+        }
+    }
+}
